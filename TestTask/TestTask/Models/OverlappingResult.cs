69093f79adce64a96d4ddc3a8f31b3fa980ee9b9
Ahmed mohamed <ahmed22mohamed9584@gmail.com>
2026-07-25 16:54:07 +0300

    feat: add OverlappingResult model

---
+++ TestTask/TestTask/Models/OverlappingResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz.Models
+{
+    // Holds the result of the FIZZBUZZ detection algorithm.
+    public class OverlappingResult
+    {
+        public string OutputString { get; set; }
+        public int Count { get; set; }
+    }
+}
